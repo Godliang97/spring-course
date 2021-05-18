fc915ac192cc5e16da9fd12dbc28872c75c54d5e
godliang <2426534657@qq.com>
2021-05-18 18:57:48 +0800

    @Before前置通知的使用(创建切面类和配置文件及切入点表达式的多种写法)

---
+++ ch06-aop-aspectj/src/test/java/com/bjpowernode/MyTest01.java
@@ -0,0 +1,24 @@
+package com.bjpowernode;
+
+import com.bjpowernode.ba01.SomeService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class MyTest01 {
+
+    @Test
+    public void test01() {
+        String confing = "applicationContext.xml";
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(confing);
+        //从容器中获取目标对象
+        SomeService proxy = (SomeService) ctx.getBean("someService");
+
+        //proxy:com.sun.proxy.$Proxy8 :jdk的动态代理
+        //System.out.println("proxy:"+proxy.getClass().getName());
+        //通过代理的对象执行方法，实现目标方法执行时，增强了功能
+        proxy.doSome("lisi",20);
+
+    }
+}
