3efd20f26af4e8a960b5e8593aa4bbfd5abdefa8
godliang <2426534657@qq.com>
2021-05-18 19:14:02 +0800

    JoinPoint的简单使用

--- ch06-aop-aspectj/src/main/java/com/bjpowernode/ba01/MyAspect.java
+++ ch06-aop-aspectj/src/main/java/com/bjpowernode/ba01/MyAspect.java
@@ -1,5 +1,6 @@
 package com.bjpowernode.ba01;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -39,15 +40,38 @@ public class MyAspect {
         System.out.println("前置通知，切面功能：在目标方法之前输出执行时间:"+new Date());
     }*/
 
-    @Before(value = "execution(* *..SomeServiceImpl.*(..))")
+    /*@Before(value = "execution(* *..SomeServiceImpl.*(..))")
     public void myBefore(){
         //就是你切面要执行的功能代码
         System.out.println("前置通知，切面功能：在目标方法之前输出执行时间:"+new Date());
-    }
+    }*/
 /*
     @Before(value = "execution(* do*(..))")
     public void myBefore(){
         //就是你切面要执行的功能代码
         System.out.println("前置通知，切面功能：在目标方法之前输出执行时间:"+new Date());
     }*/
+
+
+    /**
+     * 指定通知方法中的参数：JoinPoint
+     * JoinPoint：业务方法，要加入切面功能的业务方法
+     *      作用是：可以在通知方法中获取方法执行时的信息，例如方法名称，方法的实参。
+     *      如果你的切面功能中需要用到方法的信息，就加入JoinPoint
+     *      这个JoinPoint参数的值是由框架赋予，必须是第一个位置的参数
+     */
+
+    @Before(value = "execution(void *..SomeServiceImpl.doSome(String,Integer))")
+    public void myBefore(JoinPoint jp){
+        //获取方法的完整定义
+        System.out.println("方法的签名(定义)=" + jp.getSignature());
+        System.out.println("方法的名称=" + jp.getSignature().getName());
+        //获取方法的实参
+        Object[] args = jp.getArgs();
+        for (Object arg : args) {
+            System.out.println("参数="+arg);
+        }
+        //就是你切面要执行的功能代码
+        System.out.println("=======前置通知，切面功能：在目标方法之前输出执行时间:"+new Date());
+    }
 }
